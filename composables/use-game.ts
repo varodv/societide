cec5bb00e9e1212cc95eadc47c9ffaac43ff9668
varodv <varodv@gmail.com>
2025-05-19 16:37:01 +0200

    feat(game): Integrate useDeath into useGame

--- composables/use-game.ts
+++ composables/use-game.ts
@@ -1,17 +1,30 @@
 import type { Emitted, PlayEvent } from './types';
 
-function useGame() {
+const useGame = createSharedComposable(() => {
   const { emit, reset } = useEvent();
+  const { create } = useEntity();
 
   function play() {
     reset();
-    return emit({ type: 'PLAY' })[0] as Emitted<PlayEvent>;
+    return emit(
+      { type: 'PLAY' },
+      ...Array.from({ length: 100 }, () => ({
+        type: 'BIRTH',
+        payload: {
+          individual: create({
+            parents: [],
+          }),
+        },
+      })),
+    )[0] as Emitted<PlayEvent>;
   }
+
+  useDeath().initialize();
 
   return {
     play,
   };
-}
+});
 
 export {
   useGame,
