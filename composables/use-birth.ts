4789ac41d5caf498270e2c1d431ab609a8f41346
varodv <varodv@gmail.com>
2025-05-23 21:02:58 +0200

    feat(birth): Convert magic numbers into consts

--- composables/use-birth.ts
+++ composables/use-birth.ts
@@ -1,5 +1,9 @@
 import type { BirthEvent, Collective } from './types';
 
+const BIRTH_CHANCE = 1;
+const MAX_BIRTH_AGE = 40;
+const MAX_CHILDREN = 5;
+
 function useBirth() {
   const { couples } = useCoupling();
   const { getAge, getChildren } = useIndividual();
@@ -30,10 +34,10 @@ function useBirth() {
       getAge(couple[0]) / MILLISECONDS_IN_A_YEAR,
       getAge(couple[1]) / MILLISECONDS_IN_A_YEAR,
     );
-    if (age < 40) {
-      let totalChance = 1;
-      totalChance -= (getChildren(couple[0]).length / 5) * totalChance;
-      const times = ((40 - 18) * 365) / AGE_DAYS_MULTIPLIER;
+    if (age <= MAX_BIRTH_AGE) {
+      let totalChance = BIRTH_CHANCE;
+      totalChance -= (getChildren(couple[0]).length / MAX_CHILDREN) * totalChance;
+      const times = ((MAX_BIRTH_AGE - MIN_COUPLING_AGE) * 365) / AGE_DAYS_MULTIPLIER;
       return getPartialChance(totalChance, times);
     }
     return 0;
@@ -45,5 +49,8 @@ function useBirth() {
 }
 
 export {
+  BIRTH_CHANCE,
+  MAX_BIRTH_AGE,
+  MAX_CHILDREN,
   useBirth,
 };
